445c7a34f15b8e56a3a6394e3a8341affbcccc3a
passerByBo <sxb5626@163.com>
2021-05-10 20:32:06 +0800

    类型编程

--- 实践/index.ts
+++ 实践/index.ts
@@ -1,3 +1,48 @@
+namespace test {
+    function isString(test: any): test is string {
+        return typeof test === 'string';
+    }
+
+    function exmaple(foo: string | number) {
+        if (isString(foo)) {
+            console.log(foo.length)
+        }
+    }
+
+
+    type Diff<T, U> = T extends U ? never : T;
+
+    type R = Diff<'a' | 'b', 'C' | 'd' | 'e'>
+
+    //infer关键字
+    interface User {
+        id: number;
+        name: string;
+        form?: string;
+    }
+
+    type Foo = () => User;
+
+    type ReturnType<T> = T extends (...args: any[]) => infer P ? P : never;
+
+    type R5 = ReturnType<Foo>;
+
+
+
+    class TestClass {
+        constructor(public name: string, public age: number) { }
+    }
+
+    type ConstructirParameter5<T extends new (...args: any[]) => any> = T extends new (...args: infer P) => any
+        ? P
+        : never;
+
+    type R4 = ConstructirParameter5<typeof TestClass>
+}
+
+
+
+
 
 
 
